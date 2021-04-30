2d6e8d5207ee868af9d8a4b8df8e32b120aaf051
miunmnn <esteban.principe@utec.edu.pe>
2021-04-29 23:47:02 -0500

    Update p2.cpp

--- P2/p2.cpp
+++ P2/p2.cpp
@@ -51,42 +51,23 @@ class Record {
 private:
     std::vector<Alumno> recordVec;
     std::string fileName;
-    
+    std::fstream file_obj;
+
 
 public:
+    //Cumple con el punto a)
     Record(std::string nombreArchivo):fileName(nombreArchivo){
-    }
-    void delete_(int entry_number)
-    {
-        std::fstream file_obj;
-        Alumno alumno;
-        file_obj.open(fileName, std::ios::in | std::ios::out | std::ios::binary);
-        if (file_obj.is_open())
-        {   
-            int pointer;
-            file_obj.read((char *) &pointer, sizeof(int));
-
-            file_obj.seekg(entry_number*sizeof(Alumno)+sizeof(int));
-            file_obj.read((char *) &alumno, sizeof(Alumno) );
-
-            alumno.setNextdel(pointer);
-
-            file_obj.seekg(entry_number * sizeof(Alumno)+ sizeof(int));
-            file_obj.write((char *) &alumno, sizeof(Alumno));   
-
-            file_obj.seekg(0,std::ios::beg);
-            file_obj.write((char *) &entry_number, sizeof(int));
-
-            file_obj.close();
-        }
-    }
-    
+    }  
 
     std::vector<Alumno> load(){
+        /*
         for(auto it = recordVec.begin(); it != recordVec.end(); it++){
             it->print();
         }
         return recordVec;
+        */
+
+
     }
 
     void add(Alumno record){
@@ -96,12 +77,47 @@ public:
     Alumno readRecord(int pos){
         return recordVec[pos];
     }
+
+    bool delete_(int entry_number)
+    {
+        Alumno alumno;
+        file_obj.open(fileName, std::ios::in | std::ios::out | std::ios::binary);
+        if (file_obj.is_open())
+        {   
+            //leer el encabezado
+            int pointer;
+            file_obj.read((char *) &pointer, sizeof(int));
+
+            //leer el registro
+            file_obj.seekg(entry_number*sizeof(Alumno)+sizeof(int));
+            file_obj.read((char *) &alumno, sizeof(Alumno) );
+
+            //cambiar el pointer de eliminacion
+            alumno.setNextdel(pointer);
+
+            //escribir el registro
+            file_obj.seekg(entry_number * sizeof(Alumno)+ sizeof(int));
+            file_obj.write((char *) &alumno, sizeof(Alumno));   
+
+            //escribir el pointer de eliminacion
+            file_obj.seekg(0,std::ios::beg);
+            file_obj.write((char *) &entry_number, sizeof(int));
+
+            
+            file_obj.close();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 };
 
 int main() {
-    auto FR = Record("datos2.txt");
+    auto FR = Record("datos2.bin");
     FR.load();
-    std::cout<<"\nRegistro [9]:"<<std::endl;
+    std::cout<<"\nRegistro [2]:"<<std::endl;
     FR.readRecord(2).print();
     return 0;
 }
