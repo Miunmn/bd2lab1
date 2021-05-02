29c13664b39a6883217ac866f35b74d28bc64880
miunmnn <esteban.principe@utec.edu.pe>
2021-05-01 20:51:36 -0500

    Update

--- P4/p4.cpp
+++ P4/p4.cpp
@@ -20,34 +20,22 @@ public:
         cout<< "Codigo: \n";
         cin>> codigo;
 
-        cout << "Nombre:\n";
-        cin >> nombre;
-
-        cout<< "Apellidos: \n";
-        cin>>apellidos;
-
-        cout << "Carrera: \n";
-        cin >> carrera;
-
         cout << "Ciclo: \n";
         cin >> ciclo;
 
         cout<< "Mensualidad: \n";
         cin>>mensualidad;
         
-        cout<< "nextDel \n";
-        cin>>nextDel;
+        cout<< "Observaciones \n";
+        cin>>observaciones;
     }
 
     void showData() {
         cout<<endl;
         cout<<"Codigo: "<<codigo<<endl;
-        cout << "Nombre: " << nombre<<endl;
-        cout<< "Apellidos: "<< apellidos<<endl;
-        cout << "Carrera: " << carrera<<endl;
-        cout << "Ciclo : " << ciclo<<endl;
+        cout << "Ciclo: " << ciclo<<endl;
         cout<< "Mensualidad: "<< mensualidad<<endl;
-        cout << "Nextdel : " << nextDel<<endl;
+        cout << "Observaciones: " << observaciones<<endl;
     }
 };
 
@@ -55,54 +43,85 @@ class FixedRecordFile
 {
 private:
     string fileName;    
-
+    string fileBINName;
+    std::vector<Matricula> regVec;
 public:
-    FixedRecordFile(string _fileName) {
+    FixedRecordFile(string _fileName,string _fileBINName) {
         this->fileName = _fileName;
-		auto file = file.open(fileName);
-		std::string word;
-		char c;
-		int lenght, state = 0;
-		auto matric = new Matricula;
-		auto regVec = new std::vector<Matricula>;
-		while (file.get(c)) {
-			if (c == ':') {
-				lenght = std::stoi(word);
-				word.clear();
-				for (int i = 0; i < lenght; i++) {
-					file.get(c);
-					word.push_back(c);
-				}
-				if (state == 0){
-					matric->codigo = word;
-					state++;
-				}else if (state == 1){
-					matric->ciclo = std::stoi(word);
-					state++;
-				}else if (state == 2){
-					matric->ciclo = std::stof(word);
-					state++;
-				}else if (state == 3){
-					matric->observaciones = word;
-					state = 0;
-					regVec->push_back(matric);
-					matric = new Matricula;
-				}
-				word.clear();
-			}
-			else {
-				word.push_back(c);
-			}
+        this->fileBINName = _fileBINName;
+        std::ifstream file;
+        file.open(fileName);
+        if(file.is_open())
+        {
+            std::string word;
+            char c;
+            int lenght, state = 0;
+            auto matric = new Matricula;
+            //auto regVec = new std::vector<Matricula>;
+            while (file.get(c)) {
+                if (c == ':') {
+                    lenght = std::stoi(word);
+                    word.clear();
+                    for (int i = 0; i < lenght; i++) {
+                        file.get(c);
+                        word.push_back(c);
+                    }
+                    if (state == 0){
+                        matric->codigo = word;
+                        state++;
+                    }else if (state == 1){
+                        matric->ciclo = std::stoi(word);
+                        state++;
+                    }else if (state == 2){
+                        matric->ciclo = std::stof(word);
+                        state++;
+                    }else if (state == 3){
+                        matric->observaciones = word;
+                        state = 0;
+                        regVec.push_back(*matric);
+                        matric = new Matricula;
+                    }
+                    word.clear();
+                }
+                else {
+                    word.push_back(c);
+                }
+            }
+        }
+        create_bin_file();
+    }
 
-		}
+    void create_bin_file()
+    {
+        ofstream outFile;
+        
+        outFile.open(fileBINName, ios::binary | ios::app);        
+
+        for(auto &matricula : regVec)
+        {
+            writeRecord(matricula);
+        }
+    }
+
+    std::vector<Matricula> load()
+    {
+        return regVec;
+    }
+
+    void print_vector()
+    {
+        for(auto &matricula: regVec)
+        {
+            matricula.showData();
+        }
     }
 
     /*
     * function to write in a binary file.
     */
-    void writeRecord(Alumno obj) {
+    void writeRecord(Matricula obj) {
         ofstream outFile;
-        outFile.open(fileName, ios::binary | ios::app);        
+        outFile.open(fileBINName, ios::binary | ios::app);        
         outFile.write((char *) &obj, sizeof(obj));
         outFile.close();
     }
@@ -112,19 +131,12 @@ public:
     */
     void scanAll() {
         ifstream inFile;
-        inFile.open(fileName, ios::binary);
+        inFile.open(fileBINName, ios::binary);
         //read the records
-        Alumno obj;
-        int i = 0;
+        Matricula obj;
         while (inFile.read((char *) &obj, sizeof(obj)))
         {
-            auto alumno = readRecord(0);
-            int iterator_delete = alumno.nextDel;
-            if( i== -1 || i!=iterator_delete)
-            {
                 obj.showData(); 
-            }
-            i++;
         }
         inFile.close();
     }
@@ -132,11 +144,11 @@ public:
     /*
     * function to insert a record
     */
-    void insertRecord(Alumno  obj, int pos)
+    void insertRecord(Matricula  obj, int pos)
     {
         fstream outFile;
         cout<<"size(): "<<size()<<endl;
-        outFile.open(this->fileName,ios::binary|ios::out|ios::in);
+        outFile.open(this->fileBINName,ios::binary|ios::out|ios::in);
         if (outFile.is_open()) {
             outFile.seekp(pos * sizeof(obj), ios::beg);
             outFile.write((char *) &obj, sizeof(obj));
@@ -148,11 +160,11 @@ public:
     int size() {
         int numRecords = 0;
         fstream inFile;
-        inFile.open(this->fileName, ios::in | ios::binary);
+        inFile.open(this->fileBINName, ios::in | ios::binary);
         if (inFile.is_open()) {
             inFile.seekg(0, ios::end);
             long bytes = inFile.tellg();
-            numRecords = bytes / sizeof(Alumno);
+            numRecords = bytes / sizeof(Matricula);
             inFile.close();
         } else cout << "Could not open the file.\n";
         return numRecords;
@@ -161,7 +173,7 @@ public:
     void change_next_del_bin(int pos_obj, int pos_del)
     {
         fstream inFile;
-        inFile.open(this->fileName, ios::in | ios::binary);
+        inFile.open(this->fileBINName, ios::in | ios::binary);
         if (inFile.is_open())
         {
             inFile.seekp(pos_obj * sizeof(pos_obj)+23, ios::beg);
@@ -170,16 +182,16 @@ public:
     
     }
 
-    Alumno readRecord(int pos)
+    Matricula readRecord(int pos)
     {
         fstream inFile;
-        inFile.open(this->fileName, ios::in | ios::binary);
-        Alumno obj;
+        inFile.open(this->fileBINName, ios::in | ios::binary);
+        Matricula obj;
 
         if(inFile.is_open())
         {
             //moverse hasta la posicion del alumno
-            inFile.seekg(pos * sizeof(Alumno), ios::beg);
+            inFile.seekg(pos * sizeof(Matricula), ios::beg);
             //leer el alumno
             inFile.read((char *) &obj, sizeof(obj));
             //obj.showData();
@@ -190,86 +202,16 @@ public:
 
     bool delete_(int pos)
     {
-        auto alumno = readRecord(0);
-        int iterator_delete = alumno.nextDel;
-        int i = 0;
-        while(iterator_delete != -1)
-        {
-            alumno = readRecord(iterator_delete);
-            iterator_delete = alumno.nextDel;
-            i++;
-        }
-        alumno.nextDel = pos;
-        //change_next_del_bin(i,pos);
-        insertRecord(alumno,i);
+
     }
 };
 
-std::vector<Alumno> alumnos(string nombreArchivo)
-{     
-    std::vector<Alumno> retorno;
-    std::ifstream file;
-    std::string line;
-    Alumno* alumn;
-    int i;
-    file.open(nombreArchivo);
-    while(getline(file,line)){
-        alumn = new Alumno;
-
-        //Codigo
-        std::string aux_string = (line.substr(0,5));
-        std::memcpy(alumn->codigo,aux_string.c_str(),aux_string.size()+1);
-        //Nombre
-        aux_string = (line.substr(5,11));
-        std::memcpy(alumn->nombre,aux_string.c_str(),aux_string.size()+1);
-        //Apellidos
-        aux_string = (line.substr(16,20));
-        std::memcpy(alumn->apellidos,aux_string.c_str(),aux_string.size()+1);
-        //Carrera
-        aux_string = (line.substr(36,15));
-        std::memcpy(alumn->carrera,aux_string.c_str(),aux_string.size()+1);
-        //Ciclo
-        alumn->ciclo = stoi(line.substr(51,1));
-        //Mensualidad
-        alumn->mensualidad = stof(line.substr(52,7));
-        //NextDel;
-        alumn->nextDel = stoi(line.substr(59,2));
-        //alumn->showData();
-
-        retorno.push_back(*alumn);
-    }
-    file.close(); 
-    return retorno;
-}
-
 
 int main() {
-    std::string file_name = "datos2.txt";
-    auto FR = FixedRecordFile("datos2.bin");
-    std::ifstream file;
-    std::string line;
-    //Alumno* alumn;
-    std::vector<Alumno> alumnos_ = alumnos(file_name);
+    FixedRecordFile FR("datos4.txt","datos4.bin");
+    vector<Matricula> matriculas = FR.load();
+    FR.print_vector();
+    //FR.scanAll();
 
-    for(auto &alumno_ : alumnos_)
-    {
-        FR.writeRecord(alumno_);
-    }
-    cout<<"===================FR.scanAll()==============="<<endl<<endl<<endl;
-    FR.scanAll();
-    cout<<endl<<endl<<endl;
-    cout<<"==================FR.delete_(4)==============="<<endl<<endl<<endl;
-    FR.delete_(4);
-    cout<<endl<<endl<<endl;
-    cout<<"==================FR.delete_(2)==============="<<endl<<endl<<endl;
-    FR.delete_(2);
-    cout<<endl<<endl<<endl;
-    cout<<"==================FR.delete_(6)==============="<<endl<<endl<<endl;
-    FR.delete_(6);
-    cout<<endl<<endl<<endl;
-    cout<<"===================FR.scanAll()==============="<<endl<<endl<<endl;
-    cout<<"FR.size(): "<<FR.size();
-    FR.scanAll();
-    cout<<endl<<endl<<endl;
     return 0;
 }
